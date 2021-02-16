d4c3f140de09b5755659dc86c399228267da7df5
kokoamartin <75366670+kokoamartin@users.noreply.github.com>
2021-02-16 23:10:18 +0800

    Update Navbar.js

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -4,8 +4,8 @@ export default function Navbar() {
   return (
     <nav>
       <div className="nav-content">
-        <h1>Gdrive index</h1>
-        <a className="nav-link-github" target="_blank" rel="noopener noreferrer" href="https://wwww.angprobinsyano.com">
+        <h1>RYANIGHT</h1>
+        <a className="nav-link-github" target="_blank" rel="noopener noreferrer" href="https://www.angprobinsyano.com">
           <ion-icon name="logo-github" />
         </a>
       </div>
